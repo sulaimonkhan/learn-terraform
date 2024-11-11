e626d38aa1f2f1610292328ba5f4fd904337ea65
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-11-11 22:10:14 +0530

    terraform

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,4 +1,8 @@
 resource  "aws_instance" "frontend" {
   ami            = "ami-09c813fb71547fc4f"
   instance_type  = "t3.small"
+  vpc_security_group_ids = ["sg-074f03a20cc158368"]
+  tags {
+    Name = "frontend"
+  }
 }
