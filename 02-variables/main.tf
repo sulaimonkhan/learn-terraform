0218424709497c85935fec60fe398c28b552deb4
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-11-13 19:19:58 +0530

    variablrs

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -11,3 +11,52 @@ output "URL" {
 output "URL1" {
     value = "URL - ${var.URL}"
 }
+
+# Data types
+
+variable "num1" {
+    default = 10
+}
+
+variable "bool1" {
+    default = true
+}
+
+variable "str1" {
+    default = "Hello"
+}
+
+# Numbers , Booleans $ strings
+
+variable "list1" {
+    default = [
+        10,
+        20,
+        "xyz"
+        false
+    ]
+}
+
+
+# List can have combination of multiple data types also
+
+variable "map1" {
+    default = {
+        aws = {
+          trainer = "John"
+          duration = 30    
+        }
+        devops = {
+            trainer = "steve"
+            duration = 20
+        }
+    }
+}
+
+output "list_1" {
+    value = var.list1[2]
+}
+
+output "map1_aws_trainer" {
+    value = var.map1["aws"]["trainer"]
+}
