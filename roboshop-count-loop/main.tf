dbdd311c0e5f9339e801242b9036809bc1e6680a
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-11-14 00:05:31 +0530

    creating instances

---
+++ roboshop-count-loop/main.tf
@@ -0,0 +1,33 @@
+variable "instances" {
+  default = [
+     "frontend",
+     "cart",
+     "catalogue",
+     "user",
+     "shipping",
+     "payment",
+     "mysql",
+     "mongodb",
+     "rabbitmq",
+     "redis",
+  ]    
+}
+
+resource  "aws_instance" "instances" {
+  count = length(var.instances)  
+  ami            = "ami-09c813fb71547fc4f"
+  instance_type  = "t3.small"
+  vpc_security_group_ids = ["sg-074f03a20cc158368"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+resource "aws_route53_record" "record" {
+  count = length(var.instances)  
+  zone_id = "Z0069852OQ5LT6MOSANJ"
+  name    = "${var.instances[count.index]}-dev.sulaimondevopsb72.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.instance[count.index].private_ip]
+}
