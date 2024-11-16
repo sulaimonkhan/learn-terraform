c4e1d063a1f3c312588b0047eb6fedda910935a3
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-11-16 13:07:26 +0530

    module

--- roboshop-module/module/main.tf
+++ roboshop-module/module/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z0069852OQ5LT6MOSANJ"
-  name    = "${var.component_name}-dev.rsulaimondevopsb72.online"
+  name    = "${var.component_name}-dev.sulaimondevopsb72.online"
   type    = "A"
   ttl     = "30"
   records = [aws_instance.instance.private_ip]
