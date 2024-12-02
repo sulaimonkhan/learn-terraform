53283c517e99ba36af085a31950873e7b4f8e75a
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-12-02 20:59:08 +0530

    06-module

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -9,9 +9,8 @@ variable "instances" {
 module "ec2" {
   count = length(var.instances)
   source = "./ec2"
-  instance_name = var.instances[count.index]
+  instance_name  = var.instances[count.index]
 }
-
 
 #module "route53" {
 #  source = "./route53"    
