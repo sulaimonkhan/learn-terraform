1a9ca134abef5f9b4c1196cecfe35db50ddd187d
sulaimonkhan <sulaimonkhan04@gmail.com>
2025-03-03 16:02:36 +0530

    apply

--- 08-multi-env-code-structure/modules/ec2/main.tf
+++ 08-multi-env-code-structure/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-074872c263ca83ca5]
+  vpc_security_group_ids = ["sg-074872c263ca83ca5"]
   tags = {
     Name = "test-${var.env}"
   }
